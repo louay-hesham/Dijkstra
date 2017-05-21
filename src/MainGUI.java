c506a8777c614dc4c45c3b2c4209ee7cbf5ff454
louay-hesham <louay.hesham@gmail.com>
2017-05-21 22:19:25 +0200

    GUI logic

--- src/MainGUI.java
+++ src/MainGUI.java
@@ -1,4 +1,12 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class MainGUI extends JDialog {
     private JPanel contentPane;
@@ -8,16 +16,63 @@ public class MainGUI extends JDialog {
     private JTextArea outputTextArea;
     private JButton startDijkstraIndomieButton;
 
-    public MainGUI() {
-        setContentPane(contentPane);
+    private File fileForOperations;
+
+    private MainGUI() {
         setModal(true);
+        setContentPane(contentPane);
+        filePathTextField.setEditable(false);
+        inputTextArea.setEditable(false);
+        outputTextArea.setEditable(false);
+        inputTextArea.setFont(new Font("monospaced", Font.PLAIN, 12));
+        outputTextArea.setFont(new Font("monospaced", Font.PLAIN, 12));
+        initComponents();
     }
 
-
     public static void main(String[] args) {
-        MainGUI dialog = new MainGUI();
-        dialog.pack();
-        dialog.setVisible(true);
-        System.exit(0);
+        setUIFlavour();
+        JFrame frame = new JFrame("AVL Dictionary");
+        frame.setContentPane(new MainGUI().contentPane);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private void initComponents() {
+        chooseFileButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+                if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                    fileForOperations = fileChooser.getSelectedFile();
+                    filePathTextField.setText(fileForOperations.getAbsolutePath());
+                    StringBuilder sb = new StringBuilder();
+                    try {
+                        List<String> fileLines = Files.readAllLines(Paths.get(fileForOperations.getPath()));
+                        for (String line : fileLines) {
+                            sb.append(line).append("\n");
+                        }
+                        inputTextArea.setText(sb.toString());
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        startDijkstraIndomieButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //start Dijkstra
+            }
+        });
+    }
+
+    private static void setUIFlavour() {
+        try {
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
