58452424325f0a8faba83331e9930802bb936783
louay-hesham <louay.hesham@gmail.com>
2017-05-25 01:55:01 +0200

    altered methods to be able to use the built in test

--- src/MainGUI.java
+++ src/MainGUI.java
@@ -19,6 +19,7 @@ public class MainGUI extends JDialog {
     private JButton startDijkstraButton;
 
     private File fileForOperations;
+    private boolean firstRun = true;
 
     private MainGUI() {
         setModal(true);
@@ -55,17 +56,7 @@ public class MainGUI extends JDialog {
                 JFileChooser fileChooser = new JFileChooser();
                 if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
                     fileForOperations = fileChooser.getSelectedFile();
-                    filePathTextField.setText(fileForOperations.getAbsolutePath());
-                    StringBuilder sb = new StringBuilder();
-                    try {
-                        List<String> fileLines = Files.readAllLines(Paths.get(fileForOperations.getPath()));
-                        for (String line : fileLines) {
-                            sb.append(line).append("\n");
-                        }
-                        inputTextArea.setText(sb.toString());
-                    } catch (IOException e1) {
-                        e1.printStackTrace();
-                    }
+                    loadFile(fileForOperations);
                 }
             }
         });
@@ -73,25 +64,56 @@ public class MainGUI extends JDialog {
         startDijkstraButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                try {
-                    Scanner sc = new Scanner(fileForOperations);
-                    int n = sc.nextInt();
-                    int[][] matrix = new int[n][n];
-                    for (int i = 0; i < n; i++) {
-                        int node = sc.nextInt();
-                        int edges = sc.nextInt();
-                        for (int j = 0; j < edges; j++) {
-                            int adjacentNode = sc.nextInt();
-                            int weight = sc.nextInt();
-                            matrix[node][adjacentNode] = weight;
-                            matrix[adjacentNode][node] = weight;
-                        }
-                    }
-                    outputTextArea.setText(Dijkstra.doDijkstra(matrix));
-                } catch (FileNotFoundException e1) {
-                    e1.printStackTrace();
-                }
+                startDijkstra(fileForOperations);
             }
         });
+    }
+
+    private void loadFile(File file) {
+        filePathTextField.setText(file.getAbsolutePath());
+        StringBuilder sb = new StringBuilder();
+        try {
+            List<String> fileLines = Files.readAllLines(Paths.get(file.getPath()));
+            for (String line : fileLines) {
+                sb.append(line).append("\n");
+            }
+            inputTextArea.setText(sb.toString());
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+    }
+
+    private void startDijkstra(File file) {
+        try {
+            Scanner sc = new Scanner(file);
+            int n = sc.nextInt();
+            int[][] matrix = new int[n][n];
+            for (int i = 0; i < n; i++) {
+                int node = sc.nextInt();
+                int edges = sc.nextInt();
+                for (int j = 0; j < edges; j++) {
+                    int adjacentNode = sc.nextInt();
+                    int weight = sc.nextInt();
+                    matrix[node][adjacentNode] = weight;
+                }
+            }
+            StringBuilder sb = new StringBuilder();
+            if (firstRun) {
+                firstRun = false;
+                sb.append("This is a test run from session 7 PDF.\n");
+                sb.append("Please note that we're looping through every node and treating is as a source\n");
+                sb.append("The nodes are given IDs as follows:\n");
+                sb.append("S -> 0\n");
+                sb.append("T -> 1\n");
+                sb.append("Y -> 2\n");
+                sb.append("X -> 3\n");
+                sb.append("Z -> 4\n\n");
+
+            }
+            sb.append(Dijkstra.doDijkstra(matrix));
+            outputTextArea.setText(sb.toString());
+        } catch (FileNotFoundException e1) {
+            e1.printStackTrace();
+        }
     }
 }
